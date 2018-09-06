ae08b61f8abd50127b896dd8542f64b80d6db6ed
Angus Woodman <angus.woodman@zapier.com>
2018-09-06 02:02:12 -0400

    Added the yellow box. THIS IS NOT PART OF THE CODE FOR THE THEME AND JUST A TESTBED FOR THE WIDGET. Ok, thanks.

--- sidebar.php
+++ sidebar.php
@@ -15,3 +15,18 @@
 <aside id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
+
+<div style="box-shadow: 5px 4px 18px rgba(0, 0,0,0.1);position:relative;background-color:#FED852;color:#444;padding:18px 18px 18px 18px;margin-right:10px;border-radius:8px">
+<img style="width:48px;position:absolute;top:-18px;right:-18px;" src="https://d33wubrfki0l68.cloudfront.net/6980792beb6c6649e56b220b4e833e7b71549529/e1878/img/logo.png">
+<p style="margin-top:0;margin-bottom:0;font-size:14px;">
+This obnoxious yellow rectangle is all up in your bid'ness to
+tell you about my latest project, <strong>Grapefruit, an app for managing your mental health.</strong>
+<br><br>
+Coming soon. Poke the eyes for details!
+<br><br>
+<button style="width:100%" onclick="window.open(
+  'https://haveagrapefruit.com',
+  '_blank' // <- This is what makes it open in a new window.
+);">👀</button>
+</p>
+</div>
