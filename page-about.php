a837a0f552ef89d363cbc39bacaafbf5fb0ee1d4
Angus Woodman <angus.woodman@gmail.com>
2020-03-15 22:19:44 -0400

    Added a few About bits

--- page-about.php
+++ page-about.php
@@ -18,7 +18,7 @@ get_header(); ?>
 
 <div class="numeral-item-container">
     <div class="numeral">b.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>B</strong>urger. <br><br>I wasn't named after a burger but I've been called Angus Burger.</p>
 </div>
 
 <div class="numeral-item-container">
@@ -33,7 +33,7 @@ get_header(); ?>
 
 <div class="numeral-item-container">
     <div class="numeral">e.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>E</strong>nreprenuer'ing. <br><br>I've started and sold a number of companies. I can't stop creating things. Though I don't think of myself as an E-word.</p>
 </div>
 
 <div class="numeral-item-container">
@@ -68,7 +68,7 @@ get_header(); ?>
 
 <div class="numeral-item-container">
     <div class="numeral">k.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>K</strong>etchup. <br><br>What I wish there was an emoji of so I could use it as the subject title or calendar invite whenever I haven't spoken to someone in a while.</p>
 </div>
 
 <div class="numeral-item-container">
@@ -78,7 +78,7 @@ get_header(); ?>
 
 <div class="numeral-item-container">
     <div class="numeral">m.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>M</strong>anagment. <br><br>I think people management is a really difficult skill, and a highly under-rated one when it comes to general job satisfaction. The first time I managed people I was absolutely terrible at it. A lot of hard lessons learned, and it's opened my eyes to a whole new world.</p>
 </div>
 
 <div class="numeral-item-container">
@@ -93,17 +93,17 @@ get_header(); ?>
 
 <div class="numeral-item-container">
     <div class="numeral">p.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>P</strong>icture. <br><br>That's my name.</p>
 </div>
 
 <div class="numeral-item-container">
     <div class="numeral">q.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>Q</strong>...uuuhhh. <br><br>I'm not great at scrabble.</p>
 </div>
 
 <div class="numeral-item-container">
     <div class="numeral">r.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>R</strong>unning... <br><br>...out of interesting things to put on this list.</p>
 </div>
 
 <div class="numeral-item-container">
@@ -118,27 +118,27 @@ get_header(); ?>
 
 <div class="numeral-item-container">
     <div class="numeral">u.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>U</strong>nsplash. <br><br>My hands are in some early photos on Unsplash, which got used everywhere. If you've used the Internet, you've seen my hands. I was one of the founders of Unsplash, but I can't take too much credit for the incredible accomplishments there!</p>
 </div>
 
 <div class="numeral-item-container">
     <div class="numeral">v.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>V</strong>ideo Games. <br><br>A creative medium I've always wanted to dedicate some time to. The variety of skills required to make a full game (art, music, writing, code, etc) is really interesting.</p>
 </div>
 
 <div class="numeral-item-container">
     <div class="numeral">w.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>W</strong>oodman. <br><br>That's my sirname, yes sir.</p>
 </div>
 
 <div class="numeral-item-container">
     <div class="numeral">x.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text">U<strong>X</strong>. <br><br>A better user experience beats a little extra profit in the long-run.</p>
 </div>
 
 <div class="numeral-item-container">
     <div class="numeral">y.</div>
-    <p class="primary-text"><strong>A</strong>ngus. <br><br>That's my name.</p>
+    <p class="primary-text"><strong>Y</strong>ouTube. <br><br>I'm a paying subcriber and it's totally worth it. I hope it's a good deal for the creators vs ads; I like to support indie creators.</p>
 </div>
 
 <div class="numeral-item-container">
@@ -147,8 +147,8 @@ get_header(); ?>
 </div>
 
 
-<p class="primary-text" style="margin-bottom:120px;">
-    And if none of that interests, you, well, <a href="/its-okay-relax/">it's okay</a>.
+<p class="tertiary-text" style="margin-bottom:120px;">
+    Well that idea got old around the letter j.
 </p>
 
 <?php get_footer(); ?>
